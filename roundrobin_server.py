a1e3308de63ce4e1ec0f782ee3e7d7f32a3922f7
MrKittt <2003madi2003@gmail.com>
2022-11-11 20:00:07 +0600

    shmus

---
+++ roundrobin_server.py
@@ -0,0 +1,78 @@
+#!/bin/python3
+#  This server is responding with a fibonacci number given by a client
+#  Asynchronous program with generators, a.k.a Round Robin
+
+
+import socket
+from fib import fib
+from select import select
+
+
+tasks = []  # Contains generators of ready sockets
+
+#  queue for sockets
+to_read = {}
+to_write = {}
+
+
+def server():
+
+    serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serversock.bind(('', 5000))
+    serversock.listen(5)
+
+    while True:
+
+        yield ('read', serversock)
+        clientsock, addr = serversock.accept()
+        print(f'Connection from {addr}')
+        tasks.append(client(clientsock))
+
+
+def client(clientsock):
+
+    while True:
+        yield ('read', clientsock)
+        message = clientsock.recv(128)
+        if not message:
+            break
+        else:
+            result = str(fib(int(message.decode())))
+            yield ('write', clientsock)
+            clientsock.send(result.encode() + b'\n')
+
+    clientsock.close()
+
+
+def event_loop():
+
+    while any([tasks, to_read, to_write]):  # While we have something to do
+
+        while not tasks:  # If there are no tasks, we add them
+
+            ready_to_read, ready_to_write, _ = select(to_read, to_write, [])
+            for sock in ready_to_read:
+                tasks.append(to_read.pop(sock))
+
+            for sock in ready_to_write:
+                tasks.append(to_write.pop(sock))
+
+        try:
+            task = tasks.pop(0)
+            #  Execute next lines of gen
+            #  Because we know that socket is ready, it won't be blocking
+            reason, sock = next(task)
+
+            #  After processing a socket, add them back to queue
+            if reason == 'read':
+                to_read[sock] = task
+            if reason == 'write':
+                to_write[sock] = task
+
+        except StopIteration:  # If a client disconnects, gen will break
+            pass
+
+
+tasks.append(server())
+event_loop()
---
+++ select_server.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+#  This server is responding with a fibonacci number given by a client
+#  Asynchronous program with a select module
+
+
+import socket
+from fib import fib
+from select import select
+
+
+to_monitor = []  # monitor which sockets are ready to process
+serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversock.bind(('localhost', 5000))
+serversock.listen()
+to_monitor.append(serversock)
+
+
+def accept_connection():
+    clientsock, addr = serversock.accept()
+    to_monitor.append(clientsock)
+    print(f'Connection received {addr}')
+
+
+def send_message(clientsock):  # return fibo num from client msg
+    message = clientsock.recv(128)
+    if not message:
+        print('Connection refused')
+        to_monitor.remove(clientsock)
+        clientsock.close()
+    result = str(fib(int(message.decode())))
+    clientsock.send(result.encode() + b'\n')
+
+
+def event_loop():  # handles control
+    while True:
+        to_read, _, _ = select(to_monitor, [], [])
+        for sock in to_read:
+            if sock is serversock:
+                accept_connection()
+            else:
+                send_message(sock)
+
+
+if __name__ == '__main__':
+    event_loop()
+
